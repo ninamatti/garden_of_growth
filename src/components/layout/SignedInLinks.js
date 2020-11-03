38b2b32c5a217485a162401aace840b100e55732
Nina Matti <ninadesireematti@gmail.com>
2020-11-03 11:28:24 +0900

    adjusted Nav links

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom'
 const SignedInLinks = () => {
   return (
     <ul className="right">
-      <li><NavLink to="/">Profile</NavLink></li>
+      {/* <li><NavLink to="/">Profile</NavLink></li> */}
       <li><NavLink to="/">Log Out</NavLink></li>
-      <li><NavLink to="/" className="btn btn-floating blue lighten-1">NM</NavLink></li>
+      <li><NavLink to="/" className="btn btn-floating green lighten-3">NM</NavLink></li>
     </ul>
   )
 }
