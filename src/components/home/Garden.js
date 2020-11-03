4a3011ce181d728299b847fa2c641975421cd454
Nina Matti <ninadesireematti@gmail.com>
2020-11-03 11:31:49 +0900

    made adjustments to garden, inventory and general styling

--- src/components/home/Garden.js
+++ src/components/home/Garden.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Inventory from './Inventory';
+import Sunflower from './Produce/Flower1'
 
 
 const Garden = () => {
   return (
     <div className="garden container">
         <div className="row">
-            <div className="farm-plot container col s5 offset-s2">
+            <div className="farm-plot container col s8">
                 <div className="row">
-                    <div className="single-plot col s5 offset-s1">plot 1</div>
+                    <div className="single-plot col s5 offset-s1"><Sunflower /></div>
                     <div className="single-plot col s5">plot 2</div>
                 </div>
                 <div className="row">
@@ -17,7 +17,7 @@ const Garden = () => {
                     <div className="single-plot col s5">plot 4</div>
                 </div>
             </div>
-            <div className="inventory container col s3 offset-s2">
+            <div className="inventory container col s3 offset-s1">
                 <Inventory />
             </div>
         </div>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ import SignedOutLinks from './SignedOutLinks';
 
 const Navbar = () => {
   return (
-    <nav className="nav-wrapper grey darken-3">
+    <nav className="nav-wrapper green darken-4">
           <div className="container">
               <Link to='/' className="brand-logo">Garden of Growth</Link>
               <SignedInLinks />
