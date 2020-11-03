e12a89f05833a5a81d3609c51d952a93f7bed527
Nina Matti <ninadesireematti@gmail.com>
2020-11-03 11:28:57 +0900

    added routes for sign in and sign up components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Garden from './components/home/Garden';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Navbar />
-      <Garden />
+      <Switch>
+        <Route exact path='/' component={Garden} />
+        <Route path='/signin' component={SignIn} />
+        <Route path='/signup' component={SignUp} />
+      </Switch>
     </div>
     </BrowserRouter>
   );
